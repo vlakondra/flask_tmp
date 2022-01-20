f5aa36af2cd71cca329b53262f73935abf7811f1
vlakondra <kondrashkin.wladimir@yandex.ru>
2022-01-20 18:03:25 +0500

    Что делать?

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)  # app - экз-р класса Flask, к-й становится членом пакета app
-print("__name__: ", __name__)
+print("__name__: ", __name__, app.instance_path, "<->", app.static_url_path)
 
 from application import routes  # app - ссылка на пакет app
